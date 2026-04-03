9cc40328ea67e9a54080a0e854567858745bba22
Fauzan Nurrachman <fauzan1812@gmail.com>
2026-04-03 11:35:51 +0700

    fix(admin): replace deprecated OSMGeoAdmin with GISModelAdmin for Django 4.0+

--- fasos/admin.py
+++ fasos/admin.py
@@ -1,11 +1,17 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.contrib.gis import admin
 from .models import MedicalFacility
 
-# Register your models here.
 @admin.register(MedicalFacility)
-class MedicalFacilityAdmin(admin.OSMGeoAdmin):
-    default_zoom = 10
+class MedicalFacilityAdmin(admin.GISModelAdmin):
+    # Konfigurasi tampilan peta default
+    gis_widget_kwargs = {
+        "attrs": {
+            "default_lon": 106.8,
+            "default_lat": -6.2,
+            "default_zoom": 10
+        }
+    }
     list_filter = ['status', 'jenis', 'tingkatan']
     list_display = ['nama', 'jenis', 'tingkatan', 'status', 'operator', 'created_at']
     search_fields = ['nama', 'alamat']

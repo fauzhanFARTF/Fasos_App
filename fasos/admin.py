2d43b9867b8dc82dc19af41ae97aa5863bb3bf48
Fauzan Nurrachman <fauzan1812@gmail.com>
2026-04-03 11:27:48 +0700

    feat(admin): register MedicalFacility with OSMGeoAdmin spatial interface

--- fasos/admin.py
+++ fasos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.gis import admin
+from .models import MedicalFacility
 
 # Register your models here.
+@admin.register(MedicalFacility)
+class MedicalFacilityAdmin(admin.OSMGeoAdmin):
+    default_zoom = 10
+    list_filter = ['status', 'jenis', 'tingkatan']
+    list_display = ['nama', 'jenis', 'tingkatan', 'status', 'operator', 'created_at']
+    search_fields = ['nama', 'alamat']

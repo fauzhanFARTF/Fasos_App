cad38e8a5b99076fa9620c506922d2ce90dabea5
Fauzan Nurrachman <fauzan1812@gmail.com>
2026-04-03 16:32:32 +0700

    feat(serializer): add serializers for all OPD-specific facility models

--- fasos/serializers.py
+++ fasos/serializers.py
@@ -1,20 +1,14 @@
 from rest_framework import serializers
-from .models import MedicalFacility
+from .models import MedicalFacility, DistrictOfficeFacility, CCTVFacility
 from django.contrib.gis.geos import Point
 
-class MedicalFacilitySerializer(serializers.ModelSerializer):
+class BaseFacilitySerializer(serializers.ModelSerializer):
     location = serializers.JSONField(required=False, allow_null=True)
     photo = serializers.ImageField(required=False, allow_null=True)
-
-    class Meta:
-        model = MedicalFacility
-        fields = '__all__'
-        read_only_fields = ['operator', 'created_at', 'updated_at']
 
     def create(self, validated_data):
         loc = validated_data.pop('location', None)
         if loc and 'coordinates' in loc:
-            # GeoJSON standard: [longitude, latitude]
             validated_data['location'] = Point(loc['coordinates'][0], loc['coordinates'][1], srid=4326)
         validated_data['operator'] = self.context['request'].user
         return super().create(validated_data)
@@ -24,3 +18,21 @@ class MedicalFacilitySerializer(serializers.ModelSerializer):
         if loc and 'coordinates' in loc:
             instance.location = Point(loc['coordinates'][0], loc['coordinates'][1], srid=4326)
         return super().update(instance, validated_data)
+
+class MedicalFacilitySerializer(BaseFacilitySerializer):
+    class Meta:
+        model = MedicalFacility
+        fields = '__all__'
+        read_only_fields = ['operator', 'date_field']
+
+class DistrictOfficeFacilitySerializer(BaseFacilitySerializer):
+    class Meta:
+        model = DistrictOfficeFacility
+        fields = '__all__'
+        read_only_fields = ['operator', 'date_field']
+
+class CCTVFacilitySerializer(BaseFacilitySerializer):
+    class Meta:
+        model = CCTVFacility
+        fields = '__all__'
+        read_only_fields = ['operator', 'date_field']
